0610d60bfd14cd4e344649a1ca4cec56a653225f
robyn <robynmakes@gmail.com>
2024-07-19 15:47:14 -0500

    Initial Commit

---
+++ src/main/java/org/robyn/Main.java
@@ -0,0 +1,24 @@
+package org.robyn;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+    @Override
+    public void setup(){
+
+    }
+
+    @Override
+    public void settings(){
+
+    }
+
+    @Override
+    public void draw(){
+
+    }
+
+    public static void main(String[] args) {
+        PApplet.main("org.robyn.Main");
+    }
+}
