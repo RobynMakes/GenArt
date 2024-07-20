5d060ebb931b9922552c50f09106b6a52725dae0
robyn <robynmakes@gmail.com>
2024-07-20 11:40:43 -0500

    Restart

--- src/test/java/org/robyn/Main.java
+++ src/test/java/org/robyn/Main.java
@@ -5,17 +5,16 @@ import processing.core.PApplet;
 public class Main extends PApplet {
     @Override
     public void setup(){
-
     }
 
     @Override
     public void settings(){
-
+        size(480, 360);
     }
 
     @Override
     public void draw(){
-
+        background(0xFF000000);
     }
 
     public static void main(String[] args) {
